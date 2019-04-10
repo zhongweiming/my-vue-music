8126204c23f1520a46942dfc083eac6200c89e40
钟伟明 <zhongweiming@zhongweimingdeMacBook-Air.local>
2019-04-10 10:47:19 +0800

    music-list finished
    music-list finished

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -20,3 +20,34 @@ export function setDate (el, name, val) {
 export function getDate (el, name) {
   return el.getAttribute('data-' + name)
 }
+
+// 处理css兼容性,自动加上前缀
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle (style) {
+  if (!vendor) {
+    return false
+  }
+
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
-      <div class="filter" ></div>
+      <div class="play-wrapper" >
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll :data="songs" class="list" ref="list"
@@ -15,6 +21,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -22,14 +31,19 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import { prefixStyle } from '../../common/js/dom'
+import Loading from 'base/loading/loading'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   name: 'music-list',
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   props: {
     bgImage: {
@@ -58,14 +72,45 @@ export default {
   methods: {
     scroll (pos) {
       this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
     }
   },
   watch: {
     scrollY (newY) {
       // 滚动距离最多不超过图片高度
       let translateY = Math.max(this.minTranslateY, newY)
-      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      // 设置 z-index 控制显示层面
+      let zIndex = 0
+      // 设置下拉放大图片
+      let scale = 1
+      // 设置一个模糊度,下拉变模糊,只有iphone 能看到效果?
+      let blur = 0
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+      const percent = Math.abs(newY / this.imageHeight)
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+      // 设置模糊度
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      // 控制显示层面
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        // 设为空即表示默认显示,如 div 就是块元素
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
     }
   },
   created () {
